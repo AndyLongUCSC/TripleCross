dcf09a2f7c67b5caf35f10ea4368215b478c6f11
AndyLongUCSC <andlong@ucsc.edu>
2019-03-05 05:24:23 -0800

    Foundation of Posing for spell casting added.

--- Assets/My Assests/PlayerOneController.cs
+++ Assets/My Assests/PlayerOneController.cs
@@ -7,6 +7,17 @@ public class PlayerOneController : MonoBehaviour
     public float speed = 5f;
     public float rotateSpeed = 90f;
 
+    public string[] Inputs;
+
+    public int InputSize = 5;
+
+    public float inputSecondTime = 2f;
+
+    public float time = 0;
+    public bool startTime = false;
+
+    public bool Inputting = false;
+
     public Animator animator;
 
     private Rigidbody rb;
@@ -23,12 +34,13 @@ public class PlayerOneController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
         counter = 0;
-
+        Inputs = new string[InputSize];
     }
 
     // Update is called once per frame
     private void Update()
     {
+       
     }
 
     void FixedUpdate()
@@ -38,6 +50,10 @@ public class PlayerOneController : MonoBehaviour
         if (axis != 0)
         {
             move = true;
+            if(Inputting == true)
+            {
+                ClearInputs();
+            }
         }
         else if (axis == 0)
         {
@@ -53,6 +69,69 @@ public class PlayerOneController : MonoBehaviour
         animator.SetFloat("HVelocity", rb.velocity.x);
         animator.SetBool("Move", move);
 
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            for(int i = 0; i < InputSize; i++)
+            {
+                if (Inputs[i] == null)
+                {
+                    if (i == 0)
+                    {
+                        animator.SetBool("Up", true);
+                        Inputs[i] = "i";
+                        startTime = true;
+                        break;
+                    }
+                    else if (i > 0 && Inputs[i - 1] != "i")
+                    {
+                        time = 0;
+                        animator.SetBool("Up", true);
+                        Inputs[i] = "i";
+                        startTime = true;
+                        break;
+                    }
+                }
+            }
+            StartCoroutine(Wait());
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            for (int i = 0; i < InputSize; i++)
+            {
+                if (Inputs[i] == null)
+                {
+                    if (i == 0)
+                    {
+                        animator.SetBool("Left", true);
+                        Inputs[i] = "j";
+                        startTime = true;
+                        break;
+                    }
+                    else if (i > 0 && Inputs[i - 1] != "j")
+                    {
+                        time = 0;
+                        animator.SetBool("Left", true);
+                        Inputs[i] = "j";
+                        startTime = true;
+                        break;
+                    }
+                }
+            }
+            StartCoroutine(Wait());
+        }
+
+        if (startTime == true)
+        {
+            time += Time.deltaTime;
+            Inputting = true;
+            if (time >= inputSecondTime)
+            {
+                ClearInputs();
+            }
+        }
+        animator.SetFloat("time", time);
+        Debug.Log(time);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -67,5 +146,37 @@ public class PlayerOneController : MonoBehaviour
     public int GetCount()
     {
         return counter;
+    }
+
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(0.2f);
+        ClearBools();
+    }
+
+    private void ClearInputs()
+    {
+        Inputting = false;
+        time = 0;
+        startTime = false;
+        for(int i = 0; i < InputSize; i++)
+        {
+            Inputs[i] = null;
+        }
+        if(move == true)
+        {
+            animator.Play("Run", 0);
+        }
+        else
+        {
+            animator.Play("Idle", 0);
+        }
+        ClearBools();
+    }
+
+    private void ClearBools()
+    {
+        animator.SetBool("Up", false);
+        animator.SetBool("Left", false);
     }
 }
